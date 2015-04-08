82458f78f21c6c5d1aa593fa946c591fa0e26480
Pavel Rashkov <pavel.r.rashkov@gmail.com>
2015-04-08 15:47:42 +0300

    add Formatting homework

---
+++ Formattiong-Homework/CSharpCode/Formatting_Homework/Formatting_Homework/EventTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Formatting_Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (EventCommands.ExecuteNextCommand()) 
+        { 
+        }
+        Console.WriteLine(Messages.output);
+    }
+}
